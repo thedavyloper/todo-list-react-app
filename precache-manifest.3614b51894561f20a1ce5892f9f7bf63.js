d4b2340a63f2d7f090bc847544113db3b1856f7c
David Akpan <daviduwana@gmail.com>
2019-12-11 01:08:50 +0100

    Updates

---
+++ precache-manifest.3614b51894561f20a1ce5892f9f7bf63.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "071dcdf096cc75ac0ef79cb550eb623c",
+    "url": "/todo-list-react-app/index.html"
+  },
+  {
+    "revision": "07ce85dc23d936a16ae4",
+    "url": "/todo-list-react-app/static/css/main.3f880f4f.chunk.css"
+  },
+  {
+    "revision": "e6c1c8651bda8a8c6785",
+    "url": "/todo-list-react-app/static/js/2.d8c4d936.chunk.js"
+  },
+  {
+    "revision": "07ce85dc23d936a16ae4",
+    "url": "/todo-list-react-app/static/js/main.abde534e.chunk.js"
+  },
+  {
+    "revision": "325af6991ef8c86faff4",
+    "url": "/todo-list-react-app/static/js/runtime~main.bc9aef26.js"
+  }
+]);
